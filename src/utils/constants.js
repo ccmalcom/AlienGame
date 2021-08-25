0ff7ea53e65b39e7d9f4e1ee1efa6f7f1e227654
Chase Malcom <ChaseCMalcom@gmail.com>
2021-08-25 13:43:35 -0400

    pt. 2 complete, everything spawns

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,11 +2,13 @@ export const skyAndGroundWidth = 5000;
 
 export const gameWidth = 800;
 
+export const gameHeight = 1200;
+
 export const createInterval = 1000;
 
 export const maxFlyingObjects = 4;
 
-export const flyingObjectsStarterYAxis = -1000;
+export const flyingObjectsStarterYAxis = -1100;
 
 export const flyingObjectsStarterPositions = [
     -300,
